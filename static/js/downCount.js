5f59cfb7e1d16482b5c168709586d8a534ed21fa
dongxw <wudy.dong@yahoo.com>
2016-04-03 14:43:52 +0800

    delet jquery of countdown,use javascript

---
+++ static/js/downCount.js
@@ -0,0 +1,32 @@
+/**
+ * downCount: Simple Countdown clock with offset
+ * Author: Wudy
+ */
+function getRTime(){ 
+var StartTime=new Date('2016/3/1 00:00:00'); //开始时间 
+var EndTime= new Date('2026/5/1 00:00:00'); //截止时间 
+var NowTime = new Date(); 
+var totalT =EndTime.getTime() - StartTime.getTime(); 
+var t =EndTime.getTime() - NowTime.getTime(); 
+/*var d=Math.floor(t/1000/60/60/24); 
+t-=d*(1000*60*60*24); 
+var h=Math.floor(t/1000/60/60); 
+t-=h*60*60*1000; 
+var m=Math.floor(t/1000/60); 
+t-=m*60*1000; 
+var s=Math.floor(t/1000);*/ 
+var totalDays=Math.floor(totalT/1000/60/60/24); 
+
+var d=Math.floor(t/1000/60/60/24); 
+var h=Math.floor(t/1000/60/60%24); 
+var m=Math.floor(t/1000/60%60); 
+var s=Math.floor(t/1000%60); 
+
+document.getElementById("days").innerHTML = d; 
+document.getElementById("hours").innerHTML = h; 
+document.getElementById("minutes").innerHTML = m; 
+document.getElementById("seconds").innerHTML = s; 
+
+document.getElementById("passdays").innerHTML = totalDays-d; 
+
+} 
